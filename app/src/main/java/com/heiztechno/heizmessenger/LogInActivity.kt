2d587074d1e8b6653532695e8bb7ddd4b1fe7b12
haaza23 <nicohaza@gmail.com>
2020-01-22 11:27:47 -0300

    Cambios de diseño y vinculo con Firebase

--- app/src/main/java/com/heiztechno/heizmessenger/LogInActivity.kt
+++ app/src/main/java/com/heiztechno/heizmessenger/LogInActivity.kt
@@ -1,13 +1,65 @@
 package com.heiztechno.heizmessenger
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+
+private lateinit var txtMail : EditText
+private lateinit var txtPass : EditText
+private lateinit var btnLogIn : Button
+private lateinit var txtRegister : TextView
+
+private lateinit var auth: FirebaseAuth
 
 class LogInActivity: AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        txtMail = findViewById(R.id.txtMail)
+        txtPass = findViewById(R.id.txtPassword)
+        btnLogIn = findViewById(R.id.btnLogIn)
+        txtRegister = findViewById(R.id.txtRegister)
+
+        btnLogIn.setOnClickListener {
+            LoginUser()
+        }
+
+        txtRegister.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun LoginUser(){
+        val mail = txtMail.text.toString()
+        val contrasenia = txtPass.text.toString()
+
+        if(mail.isEmpty() || contrasenia.isEmpty()){
+            Toast.makeText(this, "Los campos no pueden estar vacios", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d("loginActivity", "El mail es: " + mail)
+        Log.d("loginActivity", "La contraseña es: $contrasenia")
+
+        auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword(mail, contrasenia)
+            .addOnCompleteListener{
+                if (it.isSuccessful){
+                    Log.d("Firebase", "Se logeo correctamente el uid: ${it?.result?.user?.uid}")
+                    val user = auth.currentUser
+                    //updateUI
+                }else{
+                    Toast.makeText(baseContext, "Error en la autenticacion",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }

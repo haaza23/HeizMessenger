c1cb401ac41840b2ff202a27e6edd6b9f61422cd
haaza23 <nicohaza@gmail.com>
2020-01-22 00:33:24 -0300

    Agregado el layout de Log In

---
+++ app/src/main/java/com/heiztechno/heizmessenger/LogInActivity.kt
@@ -0,0 +1,13 @@
+package com.heiztechno.heizmessenger
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LogInActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/heiztechno/heizmessenger/MainActivity.kt
+++ app/src/main/java/com/heiztechno/heizmessenger/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.heiztechno.heizmessenger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
 
 private lateinit var txtUser : EditText
 private lateinit var txtMail : EditText
 private lateinit var txtPass : EditText
+private lateinit var btnRegistrar : Button
+private lateinit var txtLogIn : TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,8 +24,23 @@ class MainActivity : AppCompatActivity() {
         txtUser = findViewById(R.id.txtUsername)
         txtMail = findViewById(R.id.txtMail)
         txtPass = findViewById(R.id.txtPassword)
+        btnRegistrar = findViewById(R.id.btnRegister)
+        txtLogIn = findViewById(R.id.txtLogIn)
 
-        Log.d("mainActivity", "El mail es: " + txtMail)
-        Log.d("mainActivity", "La contraseña es $txtPass")
+        btnRegistrar.setOnClickListener {
+            val mail = txtMail.text.toString()
+            val contrasenia = txtPass.text.toString()
+
+            Log.d("mainActivity", "El mail es: " + mail)
+            Log.d("mainActivity", "La contraseña es: $contrasenia")
+        }
+
+        txtLogIn.setOnClickListener {
+            Log.d("mainActivity", "mostrar pantalla log in")
+
+            //Llamar a la actividad de Log In
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
